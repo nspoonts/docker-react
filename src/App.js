4f26ebb45d6b8374aded1c205e1c1a37dfda18cc
Nathan Spoonts <nathan.spoonts@glassdoor.com>
2021-09-20 15:43:38 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload if you want to make changes to the application.
         </p>
         <a
           className="App-link"
