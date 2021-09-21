0d8f6929d87d0049e8af17a3f6cb86c1d9cfaa97
Nathan Spoonts <nathan.spoonts@glassdoor.com>
2021-09-21 16:33:02 -0700

    expose port 80 and app changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload if you want to make changes to the application.
+          Edit <code>src/App.js</code> and save to reload if you want to make changes to the app.
         </p>
         <a
           className="App-link"
